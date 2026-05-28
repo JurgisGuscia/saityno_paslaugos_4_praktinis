9a32129d53eaa438091d779a312c9e4fd1613784
JurgisGuscia <j.d.guscia@gmail.com>
2026-05-28 22:16:37 +0300

    added database connection

--- src/server.js
+++ src/server.js
@@ -1,19 +1,26 @@
 import dotenv from 'dotenv';
 import app from './app.js';
 import { scrapeEvents } from './services/eventListScraper.js';
-
+import db from './config/db.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 //const ScrapeInterval = 600000; // 10 minutes
 const ScrapeInterval = 50000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server running on http://localhost:${PORT}`);
 
-  scrapeEvents();
+  const eventUrlList = await scrapeEvents();
 
+  console.log(eventUrlList);
   //   setInterval(() => {
   //     scrapeEvents();
   //   }, ScrapeInterval);
+  try {
+    const [rows] = await db.query('SELECT 1 AS test');
+    console.log('Database connected:', rows);
+  } catch (error) {
+    console.error('Database connection failed:', error.message);
+  }
 });
--- src/services/eventListScraper.js
+++ src/services/eventListScraper.js
@@ -1,16 +1,19 @@
 import * as cheerio from 'cheerio';
 
 export async function scrapeEvents() {
-  console.log('scraping events...');
-  const response = await fetch('https://www.kaveikti.lt/');
-  const html = await response.text();
-  const parsedHtml = cheerio.load(html);
-  const events = [];
+  try {
+    const response = await fetch('https://www.kaveikti.lt/');
+    const html = await response.text();
+    const parsedHtml = cheerio.load(html);
+    const events = [];
 
-  parsedHtml('.block.event-block').each((index, element) => {
-    const link = parsedHtml(element).find('a').attr('href');
-    if (link) events.push('https://www.kaveikti.lt' + link);
-  });
-
-  console.log(events);
+    parsedHtml('.block.event-block').each((index, element) => {
+      const link = parsedHtml(element).find('a').attr('href');
+      if (link) events.push('https://www.kaveikti.lt' + link);
+    });
+    return events;
+  } catch (error) {
+    console.error('Error fetching events:', error);
+    return [];
+  }
 }
