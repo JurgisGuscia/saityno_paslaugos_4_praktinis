5c69dfec56859a311805a82bc38efdd3a1d5642f
JurgisGuscia <j.d.guscia@gmail.com>
2026-05-28 21:16:22 +0300

    updated node to v24, added cheerio for html parsing, implemented ewventListScraper, that returns a lsit of event URLs

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import { scrapeEvents } from './services/eventListScraper.js';
+
 dotenv.config();
+
 const PORT = process.env.PORT || 3000;
+//const ScrapeInterval = 600000; // 10 minutes
+const ScrapeInterval = 50000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+
+  scrapeEvents();
+
+  //   setInterval(() => {
+  //     scrapeEvents();
+  //   }, ScrapeInterval);
 });
