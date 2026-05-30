f9459d2fdc6d870c359289423abf3e56ac6e5d14
JurgisGuscia <j.d.guscia@gmail.com>
2026-05-30 23:21:17 +0300

    added openAPI

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import eventsRouter from './routes/events.routes.js';
 
 const app = express();
+const swaggerDocument = YAML.load('./docs/openapi.yaml');
 
 app.use(express.json());
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/events', eventsRouter);
 
 export default app;
